51229ce848a2258c339c5b9b547c2343d7ea326a
Percymodder <percygiraffe@gmail.com>
2015-07-12 20:01:29 -0400

    e
    e

---
+++ src/main/java/com/soultechnamei/alchemicreactions/ReactionsMain.java
@@ -0,0 +1,21 @@
+package com.soultechnamei.alchemicreactions;
+
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ReactionsMain.MODID, version = ReactionsMain.VERSION)
+public class ReactionsMain 
+{
+	public static final String MODID = "Alchemic Reactions";
+	public static final String VERSION ="1.0";
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+}
