a82726ba1a647a0b42ec7011156647c4ef97c796
Percymodder <percygiraffe@gmail.com>
2015-07-12 23:28:26 -0400

    FRixed Lang file and added texture
    LOL

---
+++ src/main/java/com/soultechnamei/alchemicreactions/Reference.java
@@ -0,0 +1,8 @@
+package com.soultechnamei.alchemicreactions;
+
+public class Reference 
+{
+	//Version
+	public static final String MODID = "alchemic reactions";
+	public static final String Version = "1.0";
+}
