a5d04664192e016ba9bb2c4b35fd63d1f64900d6
CrazzyCreeper0000 <wesley_minecraft@yahoo.com>
2015-07-13 21:15:19 -0400

    Revert "Revert "ERROR""
    This reverts commit a561d41395598eff4ea6ad9975a0aa7cc18157ed.

--- src/main/java/com/soultechnamei/alchemicreactions/ModEventHandler.java
+++ src/main/java/com/soultechnamei/alchemicreactions/ModEventHandler.java
@@ -1,14 +1,24 @@
 package com.soultechnamei.alchemicreactions;
 
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import cpw.mods.fml.common.eventhandler.Event;
+import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
 public class ModEventHandler {
-
+	
+	int LX;
+	int LY;
+	int LZ;
+	
 	@SubscribeEvent
-	public void onClicked(PlayerInteractEvent RIGHT_CLICK_BLOCK) {
-		System.out.println("this worked");
+	public void getCordsofRightClickedBlock(PlayerInteractEvent b) {
+		int LX = b.x;
+		int LY = b.y;
+		int LZ = b.z;
 
 	}
 
---
+++ src/main/java/com/soultechnamei/alchemicreactions/items/ItemBEG.java
@@ -0,0 +1,56 @@
+package com.soultechnamei.alchemicreactions.items;
+
+import java.util.List;
+
+import com.soultechnamei.alchemicreactions.ReactionsMain;
+import com.soultechnamei.alchemicreactions.Reference;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+public class ItemBEG extends Item {
+
+	int StoredEssence = 0;
+	int MaxEssence = 10;
+	int MinEssence = 0;
+
+	public ItemBEG() {
+		setHasSubtypes(true);
+		setUnlocalizedName(Reference.MODID + "_" + "Basic Essence Gem");
+		setCreativeTab(CreativeTabs.tabMisc);
+		setTextureName(Reference.MODID + ":" + "Basic Essence Gem");
+	}
+
+	public IIcon[] icons = new IIcon[4];
+
+	@Override
+	public void registerIcons(IIconRegister reg) {
+		for (int i = 0; i < 4; i++) {
+			this.icons[i] = reg.registerIcon(ReactionsMain.MODID + ":multitexture_" + i);
+		}
+	}
+
+	@Override
+	public IIcon getIconFromDamage(int meta) {
+		if (meta > 5)
+			meta = 0;
+
+		return this.icons[meta];
+	}
+
+	@Override
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
+		for (int i = 0; i < 6; i++) {
+			list.add(new ItemStack(item, 1, i));
+		}
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		return this.getUnlocalizedName() + "_" + stack.getItemDamage();
+	}
+
+}
